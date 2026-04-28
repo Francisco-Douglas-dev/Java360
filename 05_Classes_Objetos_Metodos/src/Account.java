fece4c814c72fc6b370fa589a73765302c0bd393
Francisco Douglas <franciscodouglas77@gmail.com>
2026-04-28 06:33:23 -0300

    Estudos

--- 05_Classes_Objetos_Metodos/src/Account.java
+++ 05_Classes_Objetos_Metodos/src/Account.java
@@ -1,3 +1,5 @@
 public class Account {
     String accountNumber;
+    String accoundOWner;
+    double balance;
 }
--- 05_Classes_Objetos_Metodos/src/App.java
+++ 05_Classes_Objetos_Metodos/src/App.java
@@ -2,5 +2,15 @@ public class App {
     public static void main(String[] args){
        Account a1 = new Account();
        a1.accountNumber = "5798";
+       a1.accoundOWner = "Maria";
+       a1.balance = 200;
+
+        Account a2 = new Account();
+        a2.accountNumber = "4768";
+        a2.accoundOWner = "Pedro";
+        a2.balance = 450;
+
+        System.out.println(a1.accoundOWner);
+        System.out.println(a1.accountNumber);
     }
 }
